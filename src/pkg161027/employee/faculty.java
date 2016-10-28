1e6e5890d49f2c6819a9d4829cc998f497a1b1c0
evzha0751 <evzha0751@7LLM6Y1.Student.UGDSB.ED>
2016-10-28 13:57:36 -0400

    new commit

--- src/pkg161027/employee/faculty.java
+++ src/pkg161027/employee/faculty.java
@@ -2,7 +2,13 @@
 package pkg161027.employee;
 
 import java.util.*;
-public class faculty {
-    
+public class faculty extends UEmployee {
+    Scanner input = new Scanner (System.in);
+    private String name;
+    public faculty (){
+        System.out.print("Please enter the name of department:");
+        
+        name = input.nextLine();
+}
     
 }

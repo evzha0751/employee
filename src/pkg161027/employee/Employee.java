302c870bac068f5907fecfe484e79347fe4e7c43
evzha0751 <evzha0751@7LLM6Y1.Student.UGDSB.ED>
2016-10-28 13:43:59 -0400

    new commit

--- src/pkg161027/employee/Employee.java
+++ src/pkg161027/employee/Employee.java
@@ -1,12 +1,22 @@
 
 package pkg161027.employee;
 
-
+import java.util.*;
 public class Employee {
 
-  
     public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+        UEmployee em1,em2;
+        String name,money,output,choice=null;
         
+        while(!choice.equalsIgnoreCase("L")){
+        System.out.print("Select (f) faculty or (s) staff:"
+                + "\n (l) leave:");
+        choice = input.nextLine();
+        
+        em1= new UEmployee();
+        }
     }
     
 }

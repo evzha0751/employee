352dbc060a0ff22b8f18e5373c872aaf27f09c6b
evzha0751 <evanzhao77@hotmail.com>
2016-10-30 22:12:46 -0400

    new commit

--- src/pkg161027/employee/staff.java
+++ src/pkg161027/employee/staff.java
@@ -2,6 +2,30 @@
 package pkg161027.employee;
 
 import java.util.*;
-public class staff {
+
+/**
+ * Staff class.
+ */
+public class staff extends UEmployee{
+  Scanner input = new Scanner (System.in);
+    private String name;
     
+         /**
+	 * constructor
+	 * pre: none
+	 * post: A Name of staff has been created.
+	 */
+    public staff (){
+        System.out.print("Please enter the name of staffs");
+        name = input.nextLine();
+}
+         /** 
+	 * Returns a String of the name of staff.
+	 * pre: none
+	 * post: A string of the name of staff has 
+	 * been returned.
+	 */    
+    public String toString(){
+        return(name);
+    }  
 }
